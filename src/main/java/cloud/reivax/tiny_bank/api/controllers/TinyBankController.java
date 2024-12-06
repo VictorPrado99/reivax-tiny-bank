a3c0d15bb8462f2f0c3d25feb983404915ad27f7
VictorPrado99 <vxavierprado@gmail.com>
2024-12-06 20:32:13 +0000

    wip: open api docs
    Signed-off-by: VictorPrado99 <vxavierprado@gmail.com>

--- src/main/java/cloud/reivax/tiny_bank/api/controllers/TinyBankController.java
+++ src/main/java/cloud/reivax/tiny_bank/api/controllers/TinyBankController.java
@@ -2,18 +2,37 @@ package cloud.reivax.tiny_bank.api.controllers;
 
 import cloud.reivax.tiny_bank.api.dtos.CreateUserDto;
 import cloud.reivax.tiny_bank.api.dtos.UserDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/users")
 public interface TinyBankController {
 
-    @GetMapping("/{userId}")
+    @Operation(summary = "Get specific user by userId")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User successfully recovered"),
+            @ApiResponse(responseCode = "404", description = "User not found"),
+            @ApiResponse(responseCode = "406", description = "Invalid input provided")
+    })
+    @GetMapping(path = "/{userId}", produces = "application/json")
     ResponseEntity<UserDto> getUser(@PathVariable("userId") String userId);
 
-    @PostMapping
+    @Operation(summary = "Create a new user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "User created successfully"),
+            @ApiResponse(responseCode = "406", description = "Invalid input provided")
+    })
+    @PostMapping(consumes = "application/json", produces = "application/json")
     ResponseEntity<Void> createUser(@RequestBody CreateUserDto createUserDto);
 
+    @Operation(summary = "Disable a user")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "User disabled successfully"),
+            @ApiResponse(responseCode = "406", description = "Invalid input provided")
+    })
     @DeleteMapping("/{userId}")
     ResponseEntity<Void> deactivateUser(@PathVariable("userId") String userId);
 
--- src/main/java/cloud/reivax/tiny_bank/api/controllers/TinyBankControllerImpl.java
+++ src/main/java/cloud/reivax/tiny_bank/api/controllers/TinyBankControllerImpl.java
@@ -9,7 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 
 import java.net.URI;
@@ -18,7 +18,7 @@ import java.util.UUID;
 
 @Slf4j
 @AllArgsConstructor
-@Controller
+@RestController
 public class TinyBankControllerImpl implements TinyBankController {
 
     private final UserManagementService userManagementService;
