eb3005bc36aa95cad1c4ff27acacb40538311520
VictorPrado99 <vxavierprado@gmail.com>
2024-12-08 23:42:49 +0000

    chore: improve documentation
    Signed-off-by: VictorPrado99 <vxavierprado@gmail.com>

--- src/main/java/cloud/reivax/tiny_bank/api/dtos/accounts/AccountDto.java
+++ src/main/java/cloud/reivax/tiny_bank/api/dtos/accounts/AccountDto.java
@@ -1,11 +1,16 @@
 package cloud.reivax.tiny_bank.api.dtos.accounts;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 import java.util.List;
 import java.util.UUID;
 
 public record AccountDto(
+        @Schema(defaultValue = "9130fbf3-ef2e-46ee-ac21-c49a99b6d13b")
         UUID accountId,
+        @Schema(defaultValue = "28de0db2-2541-4b50-97e1-672fb5d386a1")
         UUID userId,
+        @Schema(defaultValue = "24.42")
         double balance,
         List<TransactionDto> transactionHistory
 ) {
--- src/main/java/cloud/reivax/tiny_bank/api/dtos/accounts/TransactionDto.java
+++ src/main/java/cloud/reivax/tiny_bank/api/dtos/accounts/TransactionDto.java
@@ -2,19 +2,22 @@ package cloud.reivax.tiny_bank.api.dtos.accounts;
 
 import cloud.reivax.tiny_bank.services.processors.TransactionType;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
 
 public record TransactionDto(
-        @Schema(enumAsRef = true, implementation = TransactionType.class)
+        @Schema(enumAsRef = true, implementation = TransactionType.class, defaultValue = "TRANSFER")
         String transactionType,
-        @Nullable
+        @Schema(defaultValue = "28de0db2-2541-4b50-97e1-672fb5d386a1")
+        @Pattern(message = "It is not an UUID", regexp = "^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$")
         String origin,
-        @Nullable
+        @Schema(defaultValue = "9130fbf3-ef2e-46ee-ac21-c49a99b6d13b")
+        @Pattern(message = "It is not an UUID", regexp = "^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$")
         String recipient,
         @Min(value = 0, message = "Negative Number not Allowed")
         @DecimalMin(value = "0.01", message = "Must have some amount")
+        @Schema(defaultValue = "24.42")
         Double amount
 ) {
 }
