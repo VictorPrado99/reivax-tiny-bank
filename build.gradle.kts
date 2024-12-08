93e73d64e4e2d13246727e5cb126245aa48c63d8
VictorPrado99 <vxavierprado@gmail.com>
2024-12-08 23:05:16 +0000

    chore: remove OpenAPI with Security && observability and security dependencies for time constraint
    Signed-off-by: VictorPrado99 <vxavierprado@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,17 +26,11 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
-//	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-//	implementation("io.micrometer:micrometer-tracing-bridge-brave")
-//	implementation("io.zipkin.reporter2:zipkin-reporter-brave")
 	compileOnly("org.projectlombok:lombok")
-//	runtimeOnly("io.micrometer:micrometer-registry-prometheus")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-//	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
-
 //	Logging
 	implementation("org.slf4j:slf4j-api:2.0.16")
 //	Validation Annotations
